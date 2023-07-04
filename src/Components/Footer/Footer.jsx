fa8a95811965a698c6102dcb388926a047c7c96d
ruuuu <rufinka_91@mail.ru>
2023-07-04 12:55:25 +0300

    add Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,22 +6,89 @@ import style from './Footer.module.scss';   // style={}
 
 // компонент
 export const Footer = () => ( 
-      <div>  
-            <Container className={style.container}>  
-                  <div>
+      <footer>  
+            <Container>  
+                  <div className={style.container}>
+                        <div className={style.category}>
+                              <h2 className={style.categoryTitle}>Каталог</h2>
+                              <ul className={style.categoryList}>
+                                    <li>
+                                          <h3 className={style.categorySubtitle}>Женское</h3>
+                                          <ul className={style.categorySublist}>
+                                                <li>
+                                                      <a className={style.link}>Бюстгальтеры</a>
+                                                </li>
+                                                <li>
+                                                      <a className={style.link}>Трусы</a>
+                                                </li>
+                                                <li>
+                                                      <a className={style.link}>Носки </a>
+                                                </li>
+                                                <li>
+                                                      <a className={style.link}>Халаты</a>
+                                                </li>
+                                                <li>
+                                                      <a className={style.link}>Термобелье</a>
+                                                </li>
+                                                <li>
+                                                      <a className={style.link}>Пижамы</a>
+                                                </li>
+                                          </ul>
+                                    </li>
+                                    <li>
+                                          <h3 className={style.categorySubtitle}>Мужское</h3>
+                                          <ul className={style.categorySublist}>
+                                                <li>
+                                                      <a className={style.link}>Трусы</a>
+                                                </li>
+                                                <li>
+                                                      <a className={style.link}>Носки </a>
+                                                </li>
+                                                <li>
+                                                      <a className={style.link}>Халаты</a>
+                                                </li>
+                                                <li>
+                                                      <a className={style.link}>Термобелье</a>
+                                                </li>
+                                                <li>
+                                                      <a className={style.link}>Пижамы</a>
+                                                </li>
+                                          </ul>
+                                    </li>
+                              </ul>
+                        </div>
 
-                  </div>
-                  <div>
-                        
-                  </div>
-                  <div>
-                        
-                  </div>
-                  <div>
-                        
+                        <div className={style.social}> 
+                              <h2 className={style.socialTitle}>Связаться с нами</h2>
+                              <p className={style.socialSubtitle}>Контакты и адреса магазинов </p>
+                              <ul className={style.socialList}>
+                                    <li>
+                                          <a className={style.socialLinkFB}></a>
+                                    </li>
+                                    <li>
+                                          <a className={style.socialLinkVK}></a>
+                                    </li>
+                              </ul>
+                        </div>
+
+                        <div className={style.contacts}>
+                              <a className={style.link} href="mailto:Inspired@gmail.com">inspired@gmail.com</a>
+                              <a className={style.link} href="tel:89304902620">8 930 490 26 20</a>
+                        </div>
+
+                        <div className={style.copyright}>
+                              <p>© INSPIRED, 2023</p>
+                        </div>
+
+                        <div className={style.development}>
+                              <ul className={style.developmentList}>
+                                    <li className={style.link}>Designer: Anastasia Ilina</li>
+                                    <li className={style.link}>Руфина</li>
+                              </ul>
+                        </div>
                   </div>
             </Container>
-      </div>
+      </footer>
       
 )
       
