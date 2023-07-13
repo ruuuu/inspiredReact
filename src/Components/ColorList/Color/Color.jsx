07ac6b9f6a4c3804fae895fad1fd3b7ca006afd6
ruuuu <rufinka_91@mail.ru>
2023-07-13 20:00:20 +0300

    add Color Componenet

---
+++ src/Components/ColorList/Color/Color.jsx
@@ -0,0 +1,12 @@
+import style from './Color.module.scss';
+import cn from "classnames";
+
+
+
+// компонент
+export const Color = ({ color, check }) => {  // color = {code: , id: , title: }
+
+      return (
+            // <li cn={style.color,  style.color:after}>{color}</li>
+      )
+};
--- src/Components/ColorList/ColorList.jsx
+++ src/Components/ColorList/ColorList.jsx
@@ -1,20 +1,21 @@
 import { useSelector } from 'react-redux';
 import style from './ColorList.module.scss';  
+import { Color } from './Color/Color.jsx';
 
 
 
+
+//компонент
 export const ColorList = ({ colors }) => { // colors - массив цветов
 
-      const { colorList } = useSelector(state => state.color);                // ответ от сервера  запишем в colorList 
+      const { colorList } = useSelector(state => state.color);                // ответ от сервера  [ {code: , id: , title: }, {} ]  запишем в colorList
       
       return (
             <ul className={style.color}>
-                 {colors.map((id, i) => {
-                        const color = colorList.find(color => color.id === id);
-                        return <Color key={id} color /> 
+                 {colors.map((id, i) => {             // colors = [ {code: , id: , title: }, {} ]
+                        const color = colorList.find(color => color.id === id);                             // вернет {code: , id: , title: }
+                        return <Color key={id} color={color?.code} check={!i} />                             // возвращаем компонент Color(<li>), props = {color, check}.   color?.code - проверяем есть ли свойстов code у color 
                  })}
             </ul>
       )
-
-
 };
